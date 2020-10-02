148db450447aade8d96bcd6379f6d636213dcc5e
Asif-91 <asifchowdhury323@gmail.com>
2020-10-02 16:34:31 +0600

    connected with my own database

---
+++ src/components/Login/firebase.config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyA3ehiHoBEC9r_mwcpXGF-xtqBiuZhQEsI",
+  authDomain: "ema-jhon-with-auth-8cfb8.firebaseapp.com",
+  databaseURL: "https://ema-jhon-with-auth-8cfb8.firebaseio.com",
+  projectId: "ema-jhon-with-auth-8cfb8",
+  storageBucket: "ema-jhon-with-auth-8cfb8.appspot.com",
+  messagingSenderId: "224644827130",
+  appId: "1:224644827130:web:3bb21f883313285ad04643"
+};
+export default firebaseConfig;
